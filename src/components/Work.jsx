0d20d9fd8d582331580a6a9a35da36a7708b0449
foreverpanda <fp4nd4@gmai.com>
2024-03-19 13:31:18 +0530

    First animation done

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,44 +1,91 @@
-import React from "react";
-import "./Navbar.css"
+import React, { useState } from "react";
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+} from "framer-motion";
+import "./Navbar.css";
 function Work() {
-  var images = [
+  const [images, setImages] = useState([
     {
       url: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/650c5f15978b05dc7f2ae554_14.webp",
-      top: "50%",
+      top: "20%",
       left: "50%",
       isActive: false,
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b41324914b470b20ec7d03_Frame%203.png",
-      top: "56%",
+      top: "26%",
       left: "44%",
       isActive: false,
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/65f1160e50447ddfb490fb59_portfolioCard_cc-02%20(1).webp",
-      top: "45%",
+      top: "30%",
       left: "56%",
       isActive: false,
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/64cd6ecc885579ba1b4ffc97_10.jpg",
-      top: "60%",
+      top: "34%",
       left: "53%",
       isActive: false,
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b2cab86e8df24e528c4441_Frame%201.png",
-      top: "43%",
+      top: "26%",
       left: "39%",
       isActive: false,
     },
     {
       url: "https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b41324914b470b20ec7d03_Frame%203.png",
-      top: "65%",
+      top: "50%",
       left: "55%",
       isActive: false,
     },
-  ];
+  ]);
+  const { scrollYProgress } = useScroll();
+
+  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
+  //   showAndHideImages(Math.floor(latest * 100));
+  // });
+
+  scrollYProgress.on("change", (data) => {
+    function imagesShow(arr) {
+      setImages((prev) =>
+        prev.map((item, index) =>
+          arr.indexOf(index) === -1
+            ? { ...item, isActive: false }
+            : { ...item, isActive: true }
+        )
+      );
+    }
+
+    switch (Math.floor(data * 100)) {
+      case 0:
+        imagesShow([]);
+        break;
+      case 1:
+        imagesShow([0]);
+        break;
+      case 2:
+        imagesShow([0, 1]);
+        break;
+      case 3:
+        imagesShow([0, 1, 2]);
+        break;
+      case 4:
+        imagesShow([0, 1, 2, 3]);
+        break;
+      case 5:
+        imagesShow([0, 1, 2, 3, 4]);
+        break;
+      case 6:
+        imagesShow([0, 1, 2, 3, 4, 5]);
+        break;
+    }
+  });
 
   return (
     <div className="w-full satoshi">
