d2b72b420fc612e02992d74b209686a08a56b269
foreverpanda <fp4nd4@gmai.com>
2024-03-26 15:32:35 +0530

    Hover effect tile

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Button from "./Button";
-
+import { useState } from "react";
 function Product({ val,mover,count }) {
+
+
+  const colors = ["bg-blue-950", "bg-purple-950", "bg-cyan-500", "bg-green-600"]; 
+  const [hovered, setHovered] = useState(false);
+
+  // Get the color for this component based on its index
+  const color = colors[count % colors.length];
+
   return (
-    <div className="w-full py-20 h-[23rem] text-white bg-zinc-900">
+    <div className={`w-full py-20 h-[23rem] ${hovered ? color : '' } text-white bg-zinc-900 transition-colors duration-300`
+    } onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}>
       <div onMouseEnter={()=>{mover(count)}} className="max-w-screen-xl mx-auto flex items-center justify-between">
         <h1 className="text-6xl capitalize font-medium">{val.title}</h1>
         <div className="dets w-1/3">
